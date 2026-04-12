f0145226139350d77075ed31054f189ebdf95080
syawaura <168122150+syawaura@users.noreply.github.com>
2026-04-12 15:52:24 +0700

    Update SavingsManagement.jsx

--- src/pages/admin/SavingsManagement.jsx
+++ src/pages/admin/SavingsManagement.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-
+import "./SavingsManagement.css";
 export default function SavingsManagement() {
   const [search, setSearch] = useState("");
 
